a6a830caf7c11f91a5f7afb679184c4f49a16b2f
Patrick Michalina <patrickmichalina@mac.com>
2018-05-22 11:47:21 -0500

    fix: wrap shebang in try-catch

--- tools/scripts/fuse-shebang.ts
+++ tools/scripts/fuse-shebang.ts
@@ -1,10 +1,15 @@
 import { Bundle } from 'fuse-box'
 import { writeFileSync, chmodSync } from 'fs'
+import { logError } from '../../src/utilities/log'
 
 const sheBang = '#!/usr/bin/env node'
 
-export default function (bundle: Bundle, absOutputPath: string) {
-  const final = `${sheBang}\n${bundle.generatedCode.toString()}`
-  writeFileSync(absOutputPath, final)
-  chmodSync(absOutputPath, '755')
+export default function(bundle: Bundle, absOutputPath: string) {
+  try {
+    const final = `${sheBang}\n${bundle.generatedCode.toString()}`
+    writeFileSync(absOutputPath, final)
+    chmodSync(absOutputPath, '755')
+  } catch (err) {
+    logError(err)
+  }
 }
