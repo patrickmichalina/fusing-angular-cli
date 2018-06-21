7788f1d94eb3c60d0a3723caa10a255e668f31f7
Patrick Michalina <patrickmichalina@mac.com>
2018-06-21 00:58:11 -0500

    fix: disable package.json reference

--- src/generators/config.gen.ts
+++ src/generators/config.gen.ts
@@ -4,13 +4,13 @@ import { FAVICON_DEFAULTS as favicon } from '../templates/favicon'
 import { FUSEBOX_DEFAULTS as fusebox } from '../templates/fusebox'
 
 const configPath = 'fusing-angular.json'
-const pkg = require(resolve('package.json'))
+// const pkg = require(resolve('package.json'))
 
 export default function generateFngConfig(path: string, overwrite = false) {
   return writeJsonFile_(
     resolve(path, configPath),
     {
-      version: pkg.version,
+      // version: pkg.version,
       favicon,
       fusebox
     },
