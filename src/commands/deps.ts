ef6f27cac4432c83d9949e2519d8065553aa80e2
Patrick Michalina <patrickmichalina@mac.com>
2018-06-21 00:49:22 -0500

    fix: disable dependency info reporting (#11)

--- src/commands/deps.ts
+++ src/commands/deps.ts
@@ -1,23 +1,23 @@
-import { log } from '../utilities/log'
-import * as pkg from '../../package.json'
+// import { log } from '../utilities/log'
+// import * as pkg from '../../package.json'
 
-function createTableString() {
-  const deps = pkg.dependencies as { readonly [key: string]: string }
-  const depsOfNote: ReadonlyArray<any> = [
-    'fuse-box',
-    'rxjs',
-    'ts-node',
-    'typesciprt'
-  ]
-  const rows = Object.keys(deps)
-    .filter(k => depsOfNote.some(b => b === k))
-    .map(k => {
-      return [k, deps[k]]
-    })
-  return rows
-}
+// function createTableString() {
+//   const deps = pkg.dependencies as { readonly [key: string]: string }
+//   const depsOfNote: ReadonlyArray<any> = [
+//     'fuse-box',
+//     'rxjs',
+//     'ts-node',
+//     'typesciprt'
+//   ]
+//   const rows = Object.keys(deps)
+//     .filter(k => depsOfNote.some(b => b === k))
+//     .map(k => {
+//       return [k, deps[k]]
+//     })
+//   return rows
+// }
 
-export default function() {
-  log('Dependencies')
-  log(createTableString())
-}
+// export default function() {
+//   log('Dependencies')
+//   log(createTableString())
+// }
