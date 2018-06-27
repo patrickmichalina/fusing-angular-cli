773c9f1e67b99b52193a5e4e0fb53a6bdc7fdc7f
Patrick Michalina <patrickmichalina@mac.com>
2018-06-27 00:03:43 -0500

    fix: use correct favicon defaults

--- src/generators/config.gen.ts
+++ src/generators/config.gen.ts
@@ -4,13 +4,11 @@ import { FAVICON_DEFAULTS as favicon } from '../templates/favicon'
 import { FUSEBOX_DEFAULTS as fusebox } from '../templates/fusebox'
 
 const configPath = 'fusing-angular.json'
-// const pkg = require(resolve('package.json'))
 
 export default function generateFngConfig(path: string, overwrite = false) {
   return writeJsonFile_(
     resolve(path, configPath),
     {
-      // version: pkg.version,
       favicon,
       fusebox,
       environment: {
