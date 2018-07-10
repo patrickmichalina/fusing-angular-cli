917ecaaee7ce6fa33885421fc9a5f3a61f9f772b
Patrick Michalina <patrickmichalina@mac.com>
2018-07-09 20:54:22 -0500

    fix: include window modules in build output

--- src/modules/util/index.ts
+++ src/modules/util/index.ts
@@ -1,3 +1,5 @@
 export { WINDOW } from './tokens'
 
 export { IWindowService, WindowService } from './window/window.service'
+export { WindowServerModule } from './window/window-server.module'
+export { WindowBrowserModule } from './window/window-browser.module'
