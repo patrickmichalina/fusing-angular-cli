5df386fd870e798e779e40382e031f1952978937
Patrick Michalina <patrickmichalina@mac.com>
2018-07-12 23:44:39 -0500

    fix(firebase): include more fb tokens

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -5,13 +5,13 @@ export { WindowServerModule } from './util/window/window-server.module'
 export { WindowBrowserModule } from './util/window/window-browser.module'
 
 export { FirebaseBrowserModule } from './firebase/browser.firebase.module'
-export {
-  FirebaseServerModule,
-  FIREBASE_USER_AUTH_TOKEN,
-  FIREBASE_DATABASE_URL
-} from './firebase/server.firebase.module'
+export { FirebaseServerModule } from './firebase/server.firebase.module'
 export {
   ServerUniversalRtDbService
 } from './firebase/server.firebase.rtdb.service'
 export { UniversalRtDbService } from './firebase/browser.firebase.rtdb.service'
 export { LRU_CACHE } from './firebase/server.firebase.module'
+export {
+  FIREBASE_USER_AUTH_TOKEN
+} from './firebase/server.firebase.rtdb.service'
+export { FIREBASE_DATABASE_URL } from './firebase/tokens'
