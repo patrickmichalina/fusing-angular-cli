88dfaaff49266868c1b024eca724d6245bf2bafc
Patrick Michalina <patrickmichalina@mac.com>
2018-07-16 18:02:37 -0500

    fix: update auth server side

--- src/modules/firebase/auth/server.auth.module.ts
+++ src/modules/firebase/auth/server.auth.module.ts
@@ -1,9 +1,45 @@
 import { NgModule } from '@angular/core'
 import { AngularFireAuth } from 'angularfire2/auth'
 import { FirebaseServerAuth } from './server.auth.service'
+import {
+  FIREBASE_AUTH_SERVER_ADMIN_APP,
+  FIREBASE_AUTH_SERVER_USER_JWT
+} from './server.common'
+import { initializeApp, credential, auth } from 'firebase-admin'
+import { EnvironmentService } from '../../environment'
+import { FIREBASE_AUTH_COOKIE_STO_KEY } from './tokens'
+import { CookieService } from '../../cookies/browser'
+
+export function fbAdminFactory(es: EnvironmentService) {
+  initializeApp({
+    credential: credential.cert({
+      projectId: es.config.FIREBASE_PROJECT_ID,
+      clientEmail: es.config.SERVER_FIREBASE_CLIENT_EMAIL,
+      privateKey: es.config.SERVER_FIREBASE_PRIVATE_KEY
+    }),
+    databaseURL: es.config.FIREBASE_DATABASE_URL
+  })
+  return auth()
+}
+
+export function getUserJwt(cs: CookieService, key: string) {
+  return cs.get(key)
+}
 
 // tslint:disable-next-line:no-class
 @NgModule({
-  providers: [{ provide: AngularFireAuth, useClass: FirebaseServerAuth }]
+  providers: [
+    { provide: AngularFireAuth, useClass: FirebaseServerAuth },
+    {
+      provide: FIREBASE_AUTH_SERVER_ADMIN_APP,
+      useFactory: fbAdminFactory,
+      deps: [EnvironmentService]
+    },
+    {
+      provide: FIREBASE_AUTH_SERVER_USER_JWT,
+      useFactory: getUserJwt,
+      deps: [CookieService, FIREBASE_AUTH_COOKIE_STO_KEY]
+    }
+  ]
 })
 export class FirebaseAuthServerModule {}
--- src/modules/firebase/auth/server.auth.service.ts
+++ src/modules/firebase/auth/server.auth.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@angular/core'
+import { Injectable, Inject } from '@angular/core'
+import {
+  FIREBASE_AUTH_SERVER_ADMIN_APP,
+  FIREBASE_AUTH_SERVER_USER_JWT
+} from './server.common'
+import { auth } from 'firebase-admin'
 import { of } from 'rxjs'
+import { flatMap } from 'rxjs/operators'
 
+function validateToken(auth: auth.Auth, jwt: string) {
+  return of(auth.verifyIdToken(jwt))
+}
+
+// tslint:disable:no-this
 // tslint:disable-next-line:no-class
 @Injectable()
 export class FirebaseServerAuth {
-  readonly user = of({})
-  readonly authState = of({})
-  readonly idToken = ''
-  readonly idTokenResult = of({})
+  constructor(
+    @Inject(FIREBASE_AUTH_SERVER_ADMIN_APP) private authAdmin: auth.Auth,
+    @Inject(FIREBASE_AUTH_SERVER_USER_JWT) private jwt: string
+  ) {}
+
+  readonly validatedToken_ = this.jwt
+    ? validateToken(this.authAdmin, this.jwt).pipe(flatMap(a => a))
+    : of(null)
+
+  readonly user = this.validatedToken_
+  readonly authState = this.validatedToken_
+  readonly idTokenResult = of(this.jwt)
+  readonly idToken = this.jwt
 }
