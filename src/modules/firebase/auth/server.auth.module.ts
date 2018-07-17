302e7a2f400070f81ff60c879e553b160728fc77
Patrick Michalina <patrickmichalina@mac.com>
2018-07-16 21:23:28 -0500

    fix(firebase): handle inline PEM certificate

--- src/modules/firebase/auth/server.auth.module.ts
+++ src/modules/firebase/auth/server.auth.module.ts
@@ -5,20 +5,29 @@ import {
   FIREBASE_AUTH_SERVER_ADMIN_APP,
   FIREBASE_AUTH_SERVER_USER_JWT
 } from './server.common'
-import { initializeApp, credential, auth } from 'firebase-admin'
+import { initializeApp, credential, auth, apps } from 'firebase-admin'
 import { EnvironmentService } from '../../environment'
 import { FIREBASE_AUTH_COOKIE_STO_KEY } from './tokens'
 import { CookieService } from '../../cookies/browser'
 
+function firebaseAdminAppAlreadyExists() {
+  return apps.length ? true : false
+}
+
+function repairInlinePem(str?: string) {
+  return str ? str.replace(/\\n/g, '\n') : ''
+}
+
 export function fbAdminFactory(es: EnvironmentService) {
-  initializeApp({
-    credential: credential.cert({
-      projectId: es.config.FIREBASE_PROJECT_ID,
-      clientEmail: es.config.SERVER_FIREBASE_CLIENT_EMAIL,
-      privateKey: es.config.SERVER_FIREBASE_PRIVATE_KEY
-    }),
-    databaseURL: es.config.FIREBASE_DATABASE_URL
-  })
+  !firebaseAdminAppAlreadyExists &&
+    initializeApp({
+      credential: credential.cert({
+        projectId: es.config.FIREBASE_PROJECT_ID,
+        clientEmail: es.config.SERVER_FIREBASE_CLIENT_EMAIL,
+        privateKey: repairInlinePem(es.config.SERVER_FIREBASE_PRIVATE_KEY)
+      }),
+      databaseURL: es.config.FIREBASE_DATABASE_URL
+    })
   return auth()
 }
 
