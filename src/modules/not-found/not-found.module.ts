dcbfdd37859be9c36765fbd6c62c68655e296705
Patrick Michalina <patrickmichalina@mac.com>
2018-07-17 21:29:42 -0500

    fix: declare not-found component

--- src/modules/not-found/not-found.module.ts
+++ src/modules/not-found/not-found.module.ts
@@ -7,6 +7,7 @@ export const routes: Routes = [{ path: '**', component: NotFoundComponent }]
 // tslint:disable-next-line:no-class
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [NotFoundComponent],
+  exports: [RouterModule, NotFoundComponent]
 })
 export class NotFoundRoutingModule {}
