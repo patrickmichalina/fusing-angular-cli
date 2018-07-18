dc7b95b52b33f24ef269603ec04a79fd6eac44c6
Patrick Michalina <patrickmichalina@mac.com>
2018-07-17 22:26:11 -0500

    fix: add loginProccess watcher

--- src/modules/firebase/auth/browser.auth.module.ts
+++ src/modules/firebase/auth/browser.auth.module.ts
@@ -1,12 +1,16 @@
 import { NgModule, Inject } from '@angular/core'
 import { AngularFireAuth } from 'angularfire2/auth'
-import { flatMap } from 'rxjs/operators'
-import { ICookieGetSet } from './app.common'
 import {
-  FIREBASE_AUTH_COOKIE_FACTORY,
-  FIREBASE_AUTH_COOKIE_STO_KEY
-} from './tokens'
+  flatMap,
+  map,
+  startWith,
+  distinctUntilChanged,
+  share
+} from 'rxjs/operators'
+import { FIREBASE_AUTH_COOKIE_STO_KEY } from './tokens'
 import { of } from 'rxjs'
+import { DOCUMENT } from '@angular/common'
+import { CookieService } from '../../cookies/browser'
 
 // tslint:disable:no-this
 
@@ -14,7 +18,7 @@ function toExtractIdTokenFromUser(user: firebase.User | null) {
   return user ? user.getIdToken() : of(undefined).toPromise()
 }
 
-function storeJwtInCookies(cs: ICookieGetSet, storageKey: string) {
+function storeJwtInCookies(cs: CookieService, storageKey: string) {
   return (jwt?: string) => {
     jwt ? cs.set(storageKey, jwt) : cs.remove(storageKey)
   }
@@ -23,13 +27,44 @@ function storeJwtInCookies(cs: ICookieGetSet, storageKey: string) {
 // tslint:disable-next-line:no-class
 @NgModule()
 export class FirebaseAuthBrowserModule {
+  readonly waitingOnLoginProcess_ = this.cs
+    .targetValueChange('waitOnAuthResponse')
+    .pipe(
+      map(a => a.value),
+      startWith(this.cs.get('waitOnAuthResponse')),
+      map(val => (val && val === true ? true : false)),
+      distinctUntilChanged(),
+      share()
+    )
+
   constructor(
     public auth: AngularFireAuth,
+    private cs: CookieService,
     @Inject(FIREBASE_AUTH_COOKIE_STO_KEY) stoKey: string,
-    @Inject(FIREBASE_AUTH_COOKIE_FACTORY) getSetFactory: ICookieGetSet
+    @Inject(DOCUMENT) private doc: HTMLDocument
   ) {
     auth.user
       .pipe(flatMap(toExtractIdTokenFromUser))
-      .subscribe(storeJwtInCookies(getSetFactory, stoKey))
+      .subscribe(storeJwtInCookies(cs, stoKey))
+
+    this.waitingOnLoginProcess_.subscribe(v => {
+      setTimeout(() => {
+        const container = this.doc.querySelector(
+          'fng-firebase-spin-container'
+        ) as HTMLDivElement | undefined
+        // tslint:disable:no-if-statement
+        if (container) {
+          if (v) {
+            container.style.display = 'block'
+          } else {
+            container.style.display = 'none'
+          }
+        }
+      }, 0)
+    })
+
+    this.auth.auth.getRedirectResult().then(() => {
+      this.cs.remove('waitOnAuthResponse')
+    })
   }
 }
