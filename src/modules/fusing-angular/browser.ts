c9bb5c2a8766ce5e90b10d124103d0478f675572
Patrick Michalina <patrickmichalina@mac.com>
2018-07-21 14:54:29 -0500

    fix: add ServiceWorkerModule to consolidated module

--- src/modules/fusing-angular/browser.ts
+++ src/modules/fusing-angular/browser.ts
@@ -3,16 +3,21 @@ import { WindowBrowserModule } from '../util/window/window-browser.module'
 import { CookiesBrowserModule } from '../cookies/browser'
 import { EnvironmentBrowserModule } from '../environment'
 import { ResponseBrowserModule } from '../response/browser'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 // tslint:disable-next-line:no-class
 @NgModule({
   imports: [
+    ServiceWorkerModule.register('/js/ngsw-worker.js', {
+      enabled: false
+    }),
     WindowBrowserModule.forRoot(),
     CookiesBrowserModule,
     EnvironmentBrowserModule,
     ResponseBrowserModule
   ],
   exports: [
+    ServiceWorkerModule,
     WindowBrowserModule,
     CookiesBrowserModule,
     EnvironmentBrowserModule,
