f9331d3d94dbc14d8e0d21ace554af5bbd7ae7e9
Patrick Michalina <patrickmichalina@mac.com>
2018-07-29 00:10:31 -0500

    fix: service worker plugin updated

--- src/fusebox/ng.sw.plugin.ts
+++ src/fusebox/ng.sw.plugin.ts
@@ -11,13 +11,15 @@ const defaults = {}
 export interface NgSwPluginOptions {}
 
 export class NgSwPluginClass {
-  public test: RegExp = new RegExp('fusing-angular/browser.js')
+  public test: RegExp = new RegExp('browser')
 
   constructor(public opts: NgSwPluginOptions = defaults) {}
 
   transform(file: File) {
     const regex = new RegExp(/enabled: false/, 'g')
-    file.contents = file.contents.replace(regex, 'enabled: true')
+    if (regex.test(file.contents)) {
+      file.contents = file.contents.replace(regex, 'enabled: true')
+    }
   }
 }
 
